99a2d81a4a3476f6b790cf6094163dcf64cc7bd7
CollyneJumah <collynejumah2010@gmail.com>
2023-11-07 11:46:03 +0300

    feat: project setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "postagram": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "postagramdd3eae11": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState, useEffect } from "react";
+// import API from Amplify library
+import { API,Auth } from "aws-amplify";
+// import query definition
+import { listTodos } from "./graphql/queries";
+// src/App.js, import the withAuthenticator component and associated CSS
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+function App({signOut, user}) {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetchPosts();
+    checkUser();
+  }, []);
+  async function fetchPosts() {
+    try {
+      const postData = await API.graphql({ query: listTodos });
+      setPosts(postData.data.listTodos.items);
+    } catch (err) {
+      console.log({ err });
+    }
+  }
+  //define the checkUser function after the existing fetchPosts() function
+  async function checkUser(){
+    const user = await Auth.currentAuthenticatedUser();
+    console.log("user:", user);
+    console.log("user attributes: ", user.attributes);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World DroidconKE 2023</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.name}</h3>
+          <p>{post.location}</p>
+          <p>{post.description}</p>
+        </div>
+      ))}
+
+      <div>
+        <button style={{padding:"5px",backgroundColor:"#000",color:"white"}} onClick={signOut}>sign out</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
